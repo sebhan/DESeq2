d26613ad13b49fe6bc4542e44071e5be7051bc7a
Sebastian Hansen <sebastian@fromcodetoclinic.com>
2021-04-08 12:26:54 +0200

    Create preparations.R

---
+++ preparations.R
@@ -0,0 +1,12 @@
+# set working directory
+setwd("C:/Users/sebastian.hansen/Documents/AML/DESeq2 analysis of mutation impact")
+
+# read .csv files
+ctsdata <- ???
+coldata <- ???
+
+# strip out extraneous lines to make dimensions between ctsdata and coldata the same (coldata [x1:y2] == ctsdata [y1:x2], where x1 == y1 and x2 == y2)
+# for ctsdata, strip out first column ("Geneid")
+ctsdata <- ctsdata[-c(1)]
+
+# in ctsdata, strip out extraneous patient data ("exclrRNAsAligned... ") to harmonize with coldata
