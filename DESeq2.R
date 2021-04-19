cc2b6417efdce57701316b109c446b6c571a117f
Sebastian Hansen <sebastian@fromcodetoclinic.com>
2021-04-19 13:10:55 +0200

    Update DESeq2.R

--- DESeq2.R
+++ DESeq2.R
@@ -1,7 +1,10 @@
-ctsdata <- read.csv(file = "ctsdata.csv", header=T)
-coldata <- read.csv(file = "coldata.csv", header=F)
+# read counts data (ctsdata), dropping gene name column
+ctsdata <- read.csv(file = "ctsdata.csv", header=T, stringsAsFactors=FALSE)
+ctsdata <- ctsdata[ , -1]
 
-ddsFullCountTable <- DESeqDataSetFromMatrix(
-    countData = ctsdata,
-    colData = coldata,
-    design = ~ TP53)
+# read column data (coldata)
+coldata <- read.csv(file = "coldata.csv", header=T, row.names=1)
+
+dataset <- DESeqDataSetFromMatrix(countData = ctsdata,
+                                  colData = coldata,
+                                  design = ~ 1)
