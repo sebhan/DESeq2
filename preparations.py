58e2d26618d0bfd2b74542112aeaf8952f2ac1c9
Sebastian Hansen <sebastian@fromcodetoclinic.com>
2021-04-20 19:08:13 +0200

    Update preparations.py

--- preparations.py
+++ preparations.py
@@ -1,12 +1,34 @@
 import pandas as pd
 
-#import the counts data (ctsdata) and drop the "Geneid"-column
-ctsdata = pd.read_csv("C:/Users/sebastian.hansen/Documents/AML/DESeq2 analysis of mutation impact/merged_gene_counts_stripped.csv", sep="\t", index_col=1)
+# read counts data *.csv-file
+ctsdata = pd.read_csv("C:/Users/sebastian.hansen/Documents/AML/DESeq2 analysis of mutation impact/merged_gene_counts.csv", sep="\t", index_col=False)
 ctsdata = ctsdata.drop("Geneid", axis = 1)
 
-#import the columns data (coldata) and transpose (so that ctsdata and coldata headers are the same)
+# set underscore ("_") as separator (sep)
+sep = "_"
+
+# iterate over counts data file header and strip out extraneous data ("_exclrRNAsAligned...")
+for row in ctsdata.head(1):
+    strip = row.split(sep, 1)[0] # split the column name into two at the separator ("_")
+    # print(row, "|", strip) # used to check that all names are correct
+    ctsdata = ctsdata.rename({row: strip}, axis ="columns") # append all the "stripped" names to existing header      
+    
+# identify and remove duplicate genes in counts data
+ctsdata = ctsdata.drop_duplicates(subset="gene")
+
+# read column data
 coldata = pd.read_csv("C:/Users/sebastian.hansen/Documents/AML/DESeq2 analysis of mutation impact/mutation_groups_AS_version2.csv", sep="\t", index_col=0)
 coldata = coldata.transpose()
 
 #check if all patient IDs in coldata also exist in ctsdata
 coldata.columns.intersection(ctsdata.columns)
+
+# transpose column data back to correct orientation
+coldata = coldata.transpose()
+
+#keep only NPM1 column of coldata
+coldata_stripped = coldata[["NPM1.y"]].copy()
+
+# save the column data and counts data to *.csv-files
+coldata_stripped.to_csv("C:/Users/sebastian.hansen/Documents/AML/DESeq2 analysis of mutation impact/coldata.csv", sep=",")
+ctsdata.to_csv("C:/Users/sebastian.hansen/Documents/AML/DESeq2 analysis of mutation impact/ctsdata.csv", sep=",", index=False)
