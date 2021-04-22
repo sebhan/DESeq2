03bf6cdeae9addd5a2ea921bb9014ecbc2a2a218
Sebastian Hansen <sebastian@fromcodetoclinic.com>
2021-04-22 16:15:58 +0200

    Update DESeq_analysis_and_plotting.R
    Added gene list iteration loop.

--- DESeq_analysis_and_plotting.R
+++ DESeq_analysis_and_plotting.R
@@ -35,3 +35,26 @@ res[,6] # padj
 
 # get vector of header names of dataframe "coldata"
 names(coldata) # get specific value (e.g., fifth value) via names(coldata)[5]
+
+# create data frames to append analysis values to
+df1 <- data.frame(mutationPair=character(),log2FoldChange=character(),pval=character(),padj=character(),
+                 stringsAsFactors=FALSE)
+dfTemp <- data.frame(paste(genes[1],genes[2]),0,0.05,1)
+names(dfTemp) <- c("mutationPair","log2FoldChange","pval","padj") # note: make sure that dfTemp header names match df1 headers
+
+# append fake values
+genes <- c("NPM1","FLT3","TP53") # toy gene list
+dfTemp <- data.frame(paste(genes[1],genes[2]),0,0.05,1) # "paste()" makes string from gene list values
+df1 <- rbind(df1,dfTemp)
+
+# loop for iteration over gene list
+for(i in genes)
+{
+    for(j in genes)
+    {
+        if(i != j) # i != j exludes comparison between the same gene (e.g., NPM1 and NPM1)
+            {
+            print(c(i,j))
+            }
+    }
+}
