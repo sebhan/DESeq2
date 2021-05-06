ac5969ecb2359dd6982aa9b85336b12d74825063
Sebastian Hansen <sebastian.hansen@scilifelab.se>
2021-05-06 19:44:01 +0200

    Update plotting_DESeq2_data.R

--- plotting_DESeq2_data.R
+++ plotting_DESeq2_data.R
@@ -1,6 +1,15 @@
+# take "res" DESeq2 data frame and convert to "regular" dataframe "df"
+df <- as.data.frame(res)
+
 # remove all rows with "NA" in the "pvalue" column
 df <- df[complete.cases(df[ , 5]), ]
-df$groupNA <- ifelse(is.na(df$padj), 1, 0))
+
+# iterate over df and create column "groupNA", where rows with p-adj = NA code as 0 and p-ajd =/= NA code 1
+# input column "groupNA" as factor for use as plotting categories (NA and non-NA)
+df$groupNA <- factor(ifelse(is.na(df$padj), 0, 1))
+
+# order data frame, first on column "groupNA", second on col "pvalue"
+df <- df[with(df, order(df$groupNA, df$pvalue)), ]
 
 # use ggplot to make boxplot of DESeq2 data
-ggplot(as.data.frame(res[5]), aes(x=row.names(as.data.frame(res[5])), y=pvalue)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggplot(df, aes(x = interaction(row.names(df), groupNA), y = pvalue, fill = groupNA)) + geom_bar(position="dodge", stat = "identity") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
